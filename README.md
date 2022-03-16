3978ba4f17c83577a98f01c823601d8767066a1f
Henk vd Brink <hcebrink@gmail.com>
2022-03-16 21:02:54 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python-algorithms-blob_detection
+
+
+
+
+
+```
+sudo docker build -t test .
+```
+
+```
+sudo docker run --privileged -p 5000:5000 -v /dev/bus/usb:/dev/bus/usb app
+```
--- app/app.py
+++ app/app.py
@@ -1,15 +1,57 @@
-import cv2
+import multiprocessing as mp
+import cv2, io, traceback, logging, os
+from flask import Flask, render_template, Response
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+VIDEO_SCREEN_SIZE = (640, 480)
 
-print(cap)
+def main():
+    app = Flask(__name__)
 
-while True:
-    ret, frame = cap.read()
-    # print(frame)
+    class VideoCapture(cv2.VideoCapture):
+        def __init__(self, *args, **kwargs):
+            super(VideoCapture, self).__init__(*args, **kwargs)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        def __enter__(self):
+            return self
 
-cap.release()
+        def __exit__(self, *args, **kwargs):
+            self.release()
+
+    @app.route("/")
+    def index():
+        """Video streaming home page."""
+        return render_template("index.html")
+
+    @app.route("/video_feed")
+    def video_feed():
+        """Video streaming route. Put this in the src attribute of an img tag."""
+        return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+    def gen():
+        """Video streaming generator function."""
+
+        frame_mask = np.zeros((VIDEO_SCREEN_SIZE[1], VIDEO_SCREEN_SIZE[0], 3))
+
+        while True:
+            _, frame = vc.read()
+            frame = cv2.resize(frame, VIDEO_SCREEN_SIZE)
+
+            if frame is None:
+                logging.warning(
+                    "Frame is of type NoneType, -> error with /dev/usb0 -> reset Raspberry..."
+                )
+
+            _, image_buffer = cv2.imencode(".jpg", frame)
+            io_buf = io.BytesIO(image_buffer)
+
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + io_buf.read() + b"\r\n"
+            )
+
+    with VideoCapture(0) as vc:
+        app.run(host="0.0.0.0", threaded=True)
+
+if __name__ == "__main__":
+    main()
